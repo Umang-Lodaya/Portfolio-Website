3e674c44cdec78715ecdaea384142518f2259fb9
Umang-Lodaya <lodayaumang71@gmail.com>
2022-06-05 14:20:42 +0530

    First

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+
+!(function($) {
+  "use strict";
+
+    // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  // // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
